71dbfc7492c2801034de8e1585f073e93742f476
Ben Makusha <btmakusha@gmail.com>
2016-03-03 22:59:31 -0500

    Initial Commit

---
+++ rpgoop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rpgoop
+//
+//  Created by Baynham Makusha on 3/3/16.
+//  Copyright © 2016 Baynham Makusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
